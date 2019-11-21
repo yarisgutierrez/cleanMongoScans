cddd8e19c75a153e1aaf28d6f2d2bc111e34abe4
Yaris Alex Gutierrez <yarisgutierrez@gmail.com>
2019-11-20 22:31:26 -0500

    Added connection checks

--- cms.py
+++ cms.py
@@ -2,6 +2,7 @@
 
 import sys
 import getpass
+from cryptography.fernet import Fernet
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
@@ -22,99 +23,127 @@ class tcolors:
     UNDERLINE = "\033[4m"
 
 
-# MONGO
-# SSL checks
-mongo_ssl_check = input("\nIs MongoDB using SSL (y/n)? ").lower()
+def main():
+    # MONGO
+    # SSL checks
+    mongo_ssl_check = input("\nIs MongoDB using SSL (y/n)? ").lower()
 
-if mongo_ssl_check == "y":
-    ssl_cert = input("\nSpecify path to CA file (e.g. /path/to/ca.pem): ")
+    if mongo_ssl_check == "y":
+        ssl_cert = input("\nSpecify path to CA file (e.g. /path/to/ca.pem): ")
 
-# Connection Details
-# mongo_server = input("\nEnter MongoDB Server Address: ")
-# mongo_user = input("\nEnter MongoDB User: ")
-# mongo_password = getpass.getpass("\nEnter Password for %s: " % mongo_user)
-# mongo_db = input("\nEnter the database name: ")
+    # Connection Details
+    mongo_server = input("\nEnter MongoDB Server Address: ")
+    mongo_user = input("\nEnter MongoDB User: ")
+    mongo_password = getpass.getpass("\nEnter Password for user '%s': "
+                                    % mongo_user)
+    mongo_db = input("\nEnter the database name: ")
 
-# Create the connection
-# TODO: Work with Morgan to understand how we connect to Mongo via SSL to
-# ensure we are passing the correct paramaters in the logic
-if mongo_ssl_check == "y":
-    client = MongoClient(mongo_server,
-                         username=mongo_user,
-                         password=mongo_password,
-                         ssl=True,
-                         ssl_ca_certs=ssl_cert,
-                         authSource="admin")
-else:
-    client = MongoClient("192.168.200.3",
-                         username="bigid",
-                         password="password",
-                         authSource="admin")
+    # Create the connection
+    # TODO: Work with Morgan to understand how we connect to Mongo via SSL to
+    # ensure we are passing the correct paramaters in the logic
+    if mongo_ssl_check == "y":
+        client = MongoClient(mongo_server,
+                             username=mongo_user,
+                             password=mongo_password,
+                             ssl=True,
+                             ssl_ca_certs=ssl_cert,
+                             authSource="admin")
+    else:
+        client = MongoClient(mongo_server,
+                             username=mongo_user,
+                             password=mongo_password,
+                             authSource="admin")
 
-db = client["bigid-server"]
-
-# Define the collection and search parameters
-scans_collection = db["scans"]
-scan_name = input("\nEnter Scan Profile Name: ")
-scan_date = input("\nEnter date of scan (e.g. 2019-12-31): ")
-
-# Only print certain values from the returned dictionary in order to
-# make the output more legible. Additional keys can be added if needed
-# to expand on the details show.
-# Example of some keys that may be useful: info, origin,
-# pii_summary_completed_dt, identities_scanned, etc.
-counter = 0
-try:
-    for scans in scans_collection.find({"name": scan_name}):
-        if str(scans["created_at"])[:10] == scan_date:
-            counter += 1
-            print("\n" + tcolors.BOLD + str(counter) + ") "
-                  + tcolors.ENDC, end="")
-            print(tcolors.GREEN + tcolors.BOLD + scans["name"] +
-                  tcolors.ENDC)
-            print(tcolors.WARNING + tcolors.BOLD + "_id: "
-                  + tcolors.ENDC, end="")
-            print(scans["_id"])
-            print(tcolors.BOLD + "State: " +
-                  tcolors.ENDC, end="")
-            print(scans["state"])
-            print(tcolors.BOLD + "Type of Scan: " +
-                  tcolors.ENDC, end="")
-            print(scans["type"])
-            print(tcolors.BOLD + "Created: " +
-                  tcolors.ENDC, end="")
-            print(scans["created_at"])
-            if scans["state"] == "Completed":
-                if scans["scan_progress_status"]["Completed"]:
-                    print(tcolors.BOLD + "Completed: " +
-                          tcolors.ENDC, end="")
-                    print(scans["scan_progress_status"]["Completed"])
-            print("\n")
-        elif len(scan_date) == 0:
-            raise ValueError
-except ValueError:
-    print("\nA scan date is required!\n")
-
-if scan_date:
-    auth_rem = input("Would you like to remove a scan (y/n)? ").lower()
-    if auth_rem == "y":
-        prim_id = input("Enter the _id of the scan to remove: ")
-        print(tcolors.WARNING + "\nWARNING: THIS ACTION CANNOT BE UNDONE!!" +
-              tcolors.ENDC)
-        confirm = input("Are you sure you want to remove scan _id %s (y/n)? "
-                        % prim_id).lower()
-        if confirm == "y":
-            # Delete Primary Scan
-            for ids in scans_collection.find({"_id": ObjectId(prim_id)}):
-                scans_collection.delete_many({"_id": ObjectId(prim_id)})
-
-            # Delete sub-scans for above Primary Scan
-            for ids in scans_collection.find({"parent_scan_id": prim_id}):
-                scans_collection.delete_many({"parent_scan_id": prim_id})
-        elif confirm == "n":
-            sys.exit()
-    elif auth_rem == "n":
-        print("\nExiting.\n")
+    # Test to make sure we can mov
+    try:
+        if client.server_info():
+            print(tcolors.GREEN + "\nAuthentication successul!" + tcolors.ENDC)
+        else:
+            raise Exception
+    except Exception:
+        print("\nError: Could not connect to Mongo!\n")
         sys.exit()
-else:
-    sys.exit()
+
+    db = client[mongo_db]
+
+    # Define the collection and search parameters
+    scans_collection = db["scans"]
+
+    while True:
+        scan_name = input("\nEnter Scan Profile Name: ")
+        if len(scan_name) == 0:
+            print("\nScan Profile Name cannot be blank.")
+            pass
+        else:
+            break
+
+    scan_date = input("\nEnter date of scan (e.g. 2019-12-31): ")
+
+    # Only print certain values from the returned dictionary in order to
+    # make the output more legible. Additional keys can be added if needed
+    # to expand on the details show.
+    # Example of some keys that may be useful: info, origin,
+    # pii_summary_completed_dt, identities_scanned, etc.
+    counter = 0
+    try:
+        for scans in scans_collection.find({"name": scan_name}):
+            if str(scans["created_at"])[:10] == scan_date:
+                counter += 1
+                print("\n" + tcolors.BOLD + str(counter) + ") "
+                      + tcolors.ENDC, end="")
+                print(tcolors.GREEN + tcolors.BOLD + scans["name"] +
+                      tcolors.ENDC)
+                print(tcolors.WARNING + tcolors.BOLD + "_id: "
+                      + tcolors.ENDC, end="")
+                print(scans["_id"])
+                print(tcolors.BOLD + "State: " +
+                      tcolors.ENDC, end="")
+                print(scans["state"])
+                print(tcolors.BOLD + "Type of Scan: " +
+                      tcolors.ENDC, end="")
+                print(scans["type"])
+                print(tcolors.BOLD + "Created: " +
+                      tcolors.ENDC, end="")
+                print(scans["created_at"])
+                if scans["state"] == "Completed":
+                    if scans["scan_progress_status"]["Completed"]:
+                        print(tcolors.BOLD + "Completed: " +
+                              tcolors.ENDC, end="")
+                        print(scans["scan_progress_status"]["Completed"])
+                print("\n")
+            elif len(scan_date) == 0:
+                raise ValueError
+    except ValueError:
+        print("\nA scan date is required!\n")
+
+    if scan_date:
+        auth_rem = input("Would you like to remove a scan (y/n)? ").lower()
+        if auth_rem == "y":
+            prim_id = input("Enter the _id of the scan to remove: ")
+            print(tcolors.WARNING + "\nWARNING: THIS ACTION CANNOT BE UNDONE"
+                  + tcolors.ENDC)
+            confirm = input("\nAre you sure you want to remove scan _id %s (y/n)? "
+                            % prim_id).lower()
+            if confirm == "y":
+                # Delete Primary Scan
+                for ids in scans_collection.find({"_id": ObjectId(prim_id)}):
+                    scans_collection.delete_many({"_id": ObjectId(prim_id)})
+
+                # Delete sub-scans for above Primary Scan
+                for ids in scans_collection.find({"parent_scan_id": prim_id}):
+                    scans_collection.delete_many({"parent_scan_id": prim_id})
+            elif confirm == "n":
+                sys.exit()
+        elif auth_rem == "n":
+            print("\nExiting.\n")
+            sys.exit()
+    else:
+        sys.exit()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Quitting!\n")
+        sys.exit()
